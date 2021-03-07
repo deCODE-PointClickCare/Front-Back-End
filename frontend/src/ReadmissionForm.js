b9973008ea940e944ffbf9aa78c21ac4653d6edd
Susie Son <susieson99@gmail.com>
2021-03-07 18:42:25 -0500

    add proxy

--- frontend/src/ReadmissionForm.js
+++ frontend/src/ReadmissionForm.js
@@ -63,17 +63,14 @@ class ReadmissionForm extends Component {
   handlePredictClick = (event) => {
     const formData = this.state.formData;
     this.setState({ isLoading: true });
-    fetch(
-      "https://decode-pointclickcare-backend.herokuapp.com/api/prediction",
-      {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(formData),
-      }
-    )
+    fetch("/api/prediction", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(formData),
+    })
       .then((response) => response.json())
       .then((response) => {
         history.push({
